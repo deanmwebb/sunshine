4c5f2bd5d602410eb0fc1019a14b22aaff3ed2f6
Dean Webb <deanmwebb@gmail.com>
2015-06-11 12:56:46 -0400

    Initial commit

---
+++ app/src/main/java/app/com/example/deanofthewebb/sunshine/MainActivityFragment.java
@@ -0,0 +1,38 @@
+package app.com.example.deanofthewebb.sunshine;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/46",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Rainy - 64/51",
+                "Fri - Foggy - 70/46",
+                "Sat - Sunny - 76/68",
+                "Sun - Sunny - 79/60"
+        };
+        ArrayList<String> listViews = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
