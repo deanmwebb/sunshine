88a7abeada4686fece38fe9ddba8d8dfaea70427
Dean Webb <deanmwebb@gmail.com>
2015-06-14 14:01:57 -0400

    Binding the adapter to the listView

--- app/src/main/java/app/com/example/deanofthewebb/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/com/example/deanofthewebb/sunshine/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,6 +24,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
         String[] forecastArray = {
                 "Today - Sunny - 88/63",
                 "Tomorrow - Foggy - 70/46",
@@ -31,8 +35,14 @@ public class MainActivityFragment extends Fragment {
                 "Sat - Sunny - 76/68",
                 "Sun - Sunny - 79/60"
         };
-        ArrayList<String> listViews = new ArrayList<String>(Arrays.asList(forecastArray));
+        ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView forecastView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        forecastView.setAdapter(arrayAdapter);
+
+
+        return rootView;
     }
 }
